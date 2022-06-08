8d4ac5fa0fa43feae9d82314f01e84332d7355c6
oleesir <oleesir@gmail.com>
2022-06-08 19:35:16 +0100

    fixes

--- src/utils/getGoogleUrl.ts
+++ src/utils/getGoogleUrl.ts
@@ -2,10 +2,7 @@ const getGoogleOAuthURL = () => {
 	const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
 
 	const options = {
-		redirect_uri:
-			process.env.NODE_ENV === "production"
-				? (process.env.REACT_APP_GOOGLE_OAUTH_REDIRECT_URL_NETLIFY as string)
-				: (process.env.REACT_APP_GOOGLE_OAUTH_REDIRECT_URL as string),
+		redirect_uri: process.env.REACT_APP_GOOGLE_OAUTH_REDIRECT_URL as string,
 		client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID as string,
 		access_type: "offline",
 		response_type: "code",
