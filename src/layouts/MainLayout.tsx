85aa2ca04f3efdba97efe12525b0ca26380a0ae0
oleesir <oleesir@gmail.com>
2023-09-28 13:06:44 -0400

    remove loader from mainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,21 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header.tsx';
 import Footer from '../components/Footer.tsx';
-import { useLoadUserQuery } from '../features/oliveMarketApi.tsx';
-import Loader from '../components/Loaders/Loader.tsx';
 
 const MainLayout = () => {
-  const { isLoading } = useLoadUserQuery(undefined);
   return (
     <div className='flex flex-col w-full overflow-hidden'>
-      {isLoading && <Loader />}
-      {!isLoading && (
-        <>
-          <Header />
-          <Outlet />
-          <Footer />
-        </>
-      )}
+      <Header />
+      <Outlet />
+      <Footer />
     </div>
   );
 };
