af4ac5e74e51d34ae8c7898c2f1c2215a193287e
oleesir <oleesir@gmail.com>
2023-10-27 08:14:00 -0400

    update signupAuthFields

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import AccordionLayout from '../layouts/AccordionLayout.tsx';
 
 const Footer = () => {
   return (
-    <div className='w-full bg-[#EEEFFB]  md:py-[120px] '>
+    <div className='absolute w-full bg-[#EEEFFB]  md:py-[120px]'>
       <div className=' py-5 max-w-5xl  mx-auto px-3'>
         <div className='flex flex-col md:hidden'>
           <div className='w-full mb-2 flex justify-center'>
@@ -24,19 +24,7 @@ const Footer = () => {
               </div>
             }
           </AccordionLayout>
-          <AccordionLayout title={'Customer Case'}>
-            {
-              <div className='w-full flex '>
-                <div className='flex flex-col '>
-                  <p className='text-xs text-[#8A8FB9] mb-3'>Laptops & Computers</p>
-                  <p className='text-xs text-[#8A8FB9] mb-3'>Laptops & Computers</p>
-                  <p className='text-xs text-[#8A8FB9] mb-3'>Laptops & Computers</p>
-                  <p className='text-xs text-[#8A8FB9] mb-3'>Laptops & Computers</p>
-                  <p className='text-xs text-[#8A8FB9] mb-3'>Laptops & Computers</p>
-                </div>
-              </div>
-            }
-          </AccordionLayout>
+
           <AccordionLayout title={'Customer Case'}>
             {
               <div className='w-full flex '>
@@ -52,10 +40,10 @@ const Footer = () => {
           </AccordionLayout>
         </div>
 
-        <div className=' grid-cols-4 gap-x-4 px-5 hidden md:grid'>
+        <div className=' grid-cols-3 gap-x-4 px-5 hidden md:grid'>
           <div className='flex flex-col'>
             <p className='mb-5'>
-              <span className='text-xl mb-0 font-titanOne text-[#FD665E]'>Olive</span>
+              <span className='text-xl mb-0 font-bold text-[#FD665E]'>Olive</span>
               <span className='text-base font-titanOne text-[#FD665E]'>market</span>
             </p>
             <div className='relative text-gray-600 w-full mb-3'>
@@ -73,20 +61,9 @@ const Footer = () => {
               </button>
             </div>
             <p className='text-xs text-[#8A8FB9]'>
-              17 Princess Road, London, Greater London NW1 8JR, UK
+              17 Princess Road, London, Greater London NW1 8JR
             </p>
           </div>
-          <div className='w-full flex justify-center'>
-            <div className='flex flex-col '>
-              <p className='font-medium text-sm mb-5'>Customer Case</p>
-              <p className='text-xs text-[#8A8FB9] mb-3'>Laptops & Computers</p>
-              <p className='text-xs text-[#8A8FB9] mb-3'>Laptops & Computers</p>
-              <p className='text-xs text-[#8A8FB9] mb-3'>Laptops & Computers</p>
-              <p className='text-xs text-[#8A8FB9] mb-3'>Laptops & Computers</p>
-              <p className='text-xs text-[#8A8FB9] mb-3'>Laptops & Computers</p>
-            </div>
-          </div>
-
           <div className='w-full flex justify-center'>
             <div className='flex flex-col '>
               <p className='font-medium text-sm mb-5'>Customer Case</p>
